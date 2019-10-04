4ca99d474623ad6b42de14315812d5393508635e
Alan Weng <alan@nwk--244-198.nwk.ruw.rutgers.edu>
2019-10-04 15:24:45 -0400

    added words.txt

--- ps4.py
+++ ps4.py
@@ -112,64 +112,9 @@ def build_coder(shift):
 print(build_coder(3))
 
 def build_encoder(shift):
-    """
-    Returns a dict that can be used to encode a plain text. For example, you
-    could encrypt the plain text by calling the following commands
-    >>>encoder = build_encoder(shift)
-    >>>encrypted_text = apply_coder(plain_text, encoder)
-    
-    The cipher is defined by the shift value. Ignores non-letter characters
-    like punctuation and numbers.
-
-    shift: 0 <= int < 27
-    returns: dict
-
-    Example:
-    >>> build_encoder(3)
-    {' ': 'c', 'A': 'D', 'C': 'F', 'B': 'E', 'E': 'H', 'D': 'G', 'G': 'J',
-    'F': 'I', 'I': 'L', 'H': 'K', 'K': 'N', 'J': 'M', 'M': 'P', 'L': 'O',
-    'O': 'R', 'N': 'Q', 'Q': 'T', 'P': 'S', 'S': 'V', 'R': 'U', 'U': 'X',
-    'T': 'W', 'W': 'Z', 'V': 'Y', 'Y': 'A', 'X': ' ', 'Z': 'B', 'a': 'd',
-    'c': 'f', 'b': 'e', 'e': 'h', 'd': 'g', 'g': 'j', 'f': 'i', 'i': 'l',
-    'h': 'k', 'k': 'n', 'j': 'm', 'm': 'p', 'l': 'o', 'o': 'r', 'n': 'q',
-    'q': 't', 'p': 's', 's': 'v', 'r': 'u', 'u': 'x', 't': 'w', 'w': 'z',
-    'v': 'y', 'y': 'a', 'x': ' ', 'z': 'b'}
-    (The order of the key-value pairs may be different.)
-
-    HINT : Use build_coder.
-    """
-    ### TODO.
     return build_coder(shift)
 
 def build_decoder(shift):
-    """
-    Returns a dict that can be used to decode an encrypted text. For example, you
-    could decrypt an encrypted text by calling the following commands
-    >>>encoder = build_encoder(shift)
-    >>>encrypted_text = apply_coder(plain_text, encoder)
-    >>>decrypted_text = apply_coder(plain_text, decoder)
-    
-    The cipher is defined by the shift value. Ignores non-letter characters
-    like punctuation and numbers.
-
-    shift: 0 <= int < 27
-    returns: dict
-
-    Example:
-    >>> build_decoder(3)
-    {' ': 'x', 'A': 'Y', 'C': ' ', 'B': 'Z', 'E': 'B', 'D': 'A', 'G': 'D',
-    'F': 'C', 'I': 'F', 'H': 'E', 'K': 'H', 'J': 'G', 'M': 'J', 'L': 'I',
-    'O': 'L', 'N': 'K', 'Q': 'N', 'P': 'M', 'S': 'P', 'R': 'O', 'U': 'R',
-    'T': 'Q', 'W': 'T', 'V': 'S', 'Y': 'V', 'X': 'U', 'Z': 'W', 'a': 'y',
-    'c': ' ', 'b': 'z', 'e': 'b', 'd': 'a', 'g': 'd', 'f': 'c', 'i': 'f',
-    'h': 'e', 'k': 'h', 'j': 'g', 'm': 'j', 'l': 'i', 'o': 'l', 'n': 'k',
-    'q': 'n', 'p': 'm', 's': 'p', 'r': 'o', 'u': 'r', 't': 'q', 'w': 't',
-    'v': 's', 'y': 'v', 'x': 'u', 'z': 'w'}
-    (The order of the key-value pairs may be different.)
-
-    HINT : Use build_coder.
-    """
-    ### TODO.
     return build_coder(-shift)
 
 def apply_coder(text, coder):
@@ -188,7 +133,7 @@ def apply_coder(text, coder):
     
     return user_text
 
-print(apply_coder('hello, world',build_coder(3)))
+print(apply_coder('hello, world',build_encoder(3)))
 print(apply_coder('khoor,czruog', build_decoder(3)))
         
 
